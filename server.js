c80296c3279bb518e8f198c30c1d6edbd357609e
AbaniwoM <michaelabaniwo@gmail.com>
2023-11-01 16:16:14 +0100

    updated cors

--- server.js
+++ server.js
@@ -41,6 +41,15 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./product.json');
 const port = process.env.PORT||4000;
+const cors=require("cors");
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 // console.log(data);
 // key={ image, name, description }
