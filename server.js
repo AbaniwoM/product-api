01e5ee8268ddaf98f7aa4176596a34ca72c5bc8f
AbaniwoM <michaelabaniwo@gmail.com>
2023-11-07 16:39:46 +0100

    update app

--- server.js
+++ server.js
@@ -45,10 +45,10 @@ const router = jsonServer.router('./product.json');
 const port = process.env.PORT||4000;
 const cors = require("cors");
 
-const corsOptions ={
-   origin:'http://localhost.5173/', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
+const corsOptions = {
+   origin: '*', 
+   credentials: true,            //access-control-allow-credentials:true
+   optionSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions));
@@ -62,4 +62,5 @@ server.use(router);
 server.listen(port, () => {
     console.log(`JSON Server is running on http://localhost:${port}`);
 });
+
 
