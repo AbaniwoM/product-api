5dba4d0c0ca8a9d46983dc5cba348c168e5cdc47
AbaniwoM <michaelabaniwo@gmail.com>
2023-11-08 22:43:09 +0100

    update app

--- server.js
+++ server.js
@@ -45,13 +45,17 @@ const router = jsonServer.router('./product.json');
 const port = process.env.PORT||4000;
 const cors = require("cors");
 
-const corsOptions = {
-   origin: '*', 
-   credentials: true,            //access-control-allow-credentials:true
-   optionSuccessStatus: 200,
-}
+// const corsOptions = {
+//    origin: '*', 
+//    credentials: true,            //access-control-allow-credentials:true
+//    optionSuccessStatus: 200,
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors({
+    origin: "*"
+}));
 
  // Use this after the variable declaration
 
